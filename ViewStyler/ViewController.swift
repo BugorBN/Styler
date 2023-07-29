726b8b44060bc9dc1f9d29941fcf52ad0b999281
Boris Bugor <b.bugor@headsandhands.ru>
2023-07-29 20:29:53 +0300

    Initial Commit

---
+++ ViewStyler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewStyler
+//
+//  Created by Boris Bugor on 29/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
